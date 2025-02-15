d4e052fc72d482c028a6adf42714273ff6e0c1cc
Irsyam Okta Pratama Riyadi <pratamalismee@gmail.com>
2025-02-15 16:50:51 +0700

    Add : All section

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,21 @@
+import Footer from "./components/partials/Footer"
+import Navbar from "./components/partials/Navbar"
+import ScrollUp from "./components/partials/ScrollUp"
+import About from "./components/sections/About"
+import Hero from "./components/sections/Hero"
+import InstagramInsight from "./components/sections/InstagramInsight"
+import RateCard from "./components/sections/RateCard"
 
 function App() {
   return (
     <>
-      
+      <Navbar />
+      <Hero />
+      <About />
+      <InstagramInsight />
+      <RateCard />
+      <Footer />
+      <ScrollUp />
     </>
   )
 }
