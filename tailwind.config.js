31105404b2c047b0b82ff84377aae744c8d54f6d
Irsyam Okta Pratama Riyadi <pratamalismee@gmail.com>
2025-02-15 11:50:43 +0700

    Init project

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#A59D84',
+        secondary: '#C1BAA1',
+        accent: '#F0F0D7',
+      },
+      fontFamily: {
+        'inter': ['Inter', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
+
