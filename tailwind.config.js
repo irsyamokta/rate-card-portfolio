8cccbef3967650a33261ceb3378024033b99c19d
Irsyam Okta Pratama Riyadi <pratamalismee@gmail.com>
2025-02-15 18:34:10 +0700

    Update : Tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: {
-    relative: true,
-    transform: (content) => content.replace(/taos:/g, ''),
     files: [
       "./index.html",
       "./src/**/*.{js,ts,jsx,tsx}"
@@ -21,13 +19,6 @@ export default {
       },
     },
   },
-  plugins: [
-    require('taos/plugin'),
-  ],
-  safelist: [
-    '!duration-[0ms]',
-    '!delay-[0ms]',
-    'html.js :where([class*="taos:"]:not(.taos-init))'
-  ]
+  plugins: [],
 }
 
