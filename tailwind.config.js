ff6777e10c5f3401e9f938d98a6bef5c615555a2
Irsyam Okta Pratama Riyadi <pratamalismee@gmail.com>
2025-02-15 16:48:13 +0700

    Update : Tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,33 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: {
+    relative: true,
+    transform: (content) => content.replace(/taos:/g, ''),
+    files: [
+      "./index.html",
+      "./src/**/*.{js,ts,jsx,tsx}"
+    ]
+  },
   theme: {
     extend: {
       colors: {
         primary: '#A59D84',
         secondary: '#C1BAA1',
         accent: '#F0F0D7',
+        background: '#EFEFEF'
       },
       fontFamily: {
         'inter': ['Inter', 'sans-serif'],
       },
     },
   },
-  plugins: [],
+  plugins: [
+    require('taos/plugin'),
+  ],
+  safelist: [
+    '!duration-[0ms]',
+    '!delay-[0ms]',
+    'html.js :where([class*="taos:"]:not(.taos-init))'
+  ]
 }
 
